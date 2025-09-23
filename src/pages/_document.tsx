81ffea2018d231c713cff91039d9ade432e2c749
Shoaib-Programmer <shoaibnigam422@gmail.com>
2025-09-23 08:43:29 +0000

    Fixed metadata issues

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,21 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 export default function Document() {
     return (
         <Html lang="en">
-            <Head />
+            <Head>
+                <title>Pollination Fun!</title>
+                <meta name="description" content="An educational game about bees, flowers, and pollination." />
+                <meta name="application-name" content="Honey Helpers" />
+                <meta property="og:site_name" content="Honey Helpers" />
+                <link rel="icon" href="/favicon.png" />
+            </Head>
             <body>
                 <Main />
                 <NextScript />
             </body>
+            <GoogleAnalytics gaId="G-GE1C2LW2H9" />
         </Html>
     );
 }
